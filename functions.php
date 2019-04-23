1bc76fbde9fa45fe03c726f9c97d99899dfc7445
Julia Gutkina <julia.gutkina@perfectart.com>
2019-04-23 23:21:42 +0300

    Add checking time expiration

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+function countTasks(array $tasks, $projectName) {
+    $count = 0;
+
+    foreach($tasks as $task) {
+        if (isset($task['category']) && ($task['category'] === $projectName)) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+
+function checkExpiration($date, $expirationHours = 24) {
+    $SECS_IN_HOUR = 3600;
+    $tsDate = strtotime($date);
+    $tsNow = time();
+    $tsDiff = $tsDate - $tsNow;
+    $hoursDiff = floor($tsDiff / $SECS_IN_HOUR);
+
+    return ($hoursDiff <= $expirationHours);
+}
--- index.php
+++ index.php
@@ -1,59 +1,12 @@
 <?php
+date_default_timezone_set('Asia/Jerusalem');
+
 require_once('helpers.php');
+require_once('functions.php');
+require_once('data.php');
 
 $showCompleteTasks = rand(0, 1);
-$projects = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
-$tasks = [
-    [
-        'name' => 'Собеседование в IT компании',
-        'date' => '01.12.2018',
-        'category' => 'Работа',
-        'done' => true
-    ],
-    [
-        'name' => 'Выполнить тестовое задание',
-        'date' => '	25.12.2018',
-        'category' => 'Работа',
-        'done' => false
-    ],
-    [
-        'name' => 'Сделать задание первого раздела',
-        'date' => '21.12.2018',
-        'category' => 'Учеба',
-        'done' => true
-    ],
-    [
-        'name' => 'Встреча с другом',
-        'date' => '	22.12.2018',
-        'category' => 'Входящие',
-        'done' => false
-    ],
-    [
-        'name' => 'Купить корм для кота',
-        'date' => null,
-        'category' => 'Домашние дела',
-        'done' => false
-    ],
-    [
-        'name' => 'Заказать пиццу',
-        'date' => null,
-        'category' => 'Домашние дела',
-        'done' => false
-    ]
-];
 $userName = 'Константин';
-
-function countTasks(array $tasks, $projectName) {
-    $count = 0;
-
-    foreach($tasks as $task) {
-        if (isset($task['category']) && ($task['category'] === $projectName)) {
-            $count++;
-        }
-    }
-
-    return $count;
-}
 
 $pageContent = include_template('index.php', [
     'tasks' => $tasks,
