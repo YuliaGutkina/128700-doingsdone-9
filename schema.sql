7b4196e7e9882330934bff15169dccd832df69b8
Julia Gutkina <julia.gutkina@perfectart.com>
2019-04-29 07:21:33 +0300

    Add missing fields

--- schema.sql
+++ schema.sql
@@ -4,22 +4,25 @@ CREATE DATABASE doingsdone
 USE doingsdone;
 CREATE TABLE projects (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(128)
+  name CHAR(128) NOT NULL,
+  user_id INT NOT NULL
 );
 CREATE TABLE tasks (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  dt_create DATETIME,
+  dt_create DATETIME NOT NULL DEFAULT NOW(),
   status INT DEFAULT 0,
-  name CHAR(128),
+  name CHAR(128) NOT NULL,
   file CHAR(128),
-  deadline DATETIME
+  deadline DATETIME,
+  project_id INT NOT NULL,
+  user_id INT NOT NULL
 );
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  dt_reg DATETIME,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dt_reg DATETIME NOT NULL DEFAULT NOW(),
   email CHAR(128) NOT NULL,
-  name CHAR(128),
-  password CHAR(64)
+  name CHAR(128) NOT NULL,
+  password CHAR(64) NOT NULL
 );
 CREATE UNIQUE INDEX p_name ON projects(name);
 CREATE UNIQUE INDEX email ON users(email);
